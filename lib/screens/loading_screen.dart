7f3ee8586eb8c9fd4ed744c808df31c02ae36ad6
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-05-08 12:59:44 -0500

    added geolocation package and created getLocation method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +8,18 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+
     );
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void getLocation() async{
+  try{
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+  catch(e){
+    print(e);
+  }
+}
